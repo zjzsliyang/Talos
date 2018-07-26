9ba323a7a6e88142e045ebdbdee79720934b2ad8
Yang LI <zjzsliyang@gmail.com>
2018-07-26 10:54:32 +0800

    refactor

--- talos.py
+++ talos.py
@@ -8,6 +8,7 @@ import logging
 import threading
 import subprocess
 from sklearn import svm
+from collections import defaultdict
 from stop_words import get_stop_words
 from gensim import models, similarities, corpora
 
@@ -17,6 +18,7 @@ MANNAME = 'manual'
 LSIMODEL = 'lsi.model'
 PERIODS = ['201707', '201708', '201709', '201710', '201711', '201712', '201801', '201802', '201803', '201804', '201805',
            '201806', '201807']
+
 
 
 class Activity:
@@ -52,45 +54,6 @@ class Shell_Command:
         self.description = data['d']
 
 
-def read_dataset(period: str):
-    logging.info('reading dataset from json in following period: {}'.format(period))
-    sessions = []
-    for root, dirs, files in os.walk(LOGPATH + '/' + period):
-        for name in files:
-            file_dir = os.path.join(root, name)
-            logging.debug('current read json file: {}'.format(file_dir.split('logs/')[1]))
-            with open(file_dir, encoding='utf-8') as raw_log_file:
-                data = json.load(raw_log_file)
-            sessions.append(Session(data))
-        log_file = open(LOGNAME + '_' + period + '.pickle', 'wb')
-        pickle.dump(sessions, log_file)
-        log_file.close()
-    logging.info('the size of log is {} MB'.format(sys.getsizeof(sessions) / 1000 / 1000))
-
-
-def load_dataset(multithread: bool = False) -> [str]:
-    if multithread:
-        pool = []
-        for period in PERIODS:
-            pool.append(threading.Thread(target=read_dataset, args=(period,)))
-        for thread in pool:
-            thread.start()
-        for thread in pool:
-            thread.join()
-    commands = []
-    for period in PERIODS:
-        logging.info('load dataset from pickle in following period: {}'.format(period))
-        with open(LOGNAME + '_' + period + '.pickle', 'rb') as log_file:
-            sessions = pickle.load(log_file)
-        for session in sessions:
-            for activity in session.activities:
-                if activity.command is not None:
-                    if len(activity.command.strip().split()):
-                        commands.append(activity.command.strip().split()[0])
-        logging.info('current load {} commands from pickle'.format(len(commands)))
-    return commands
-
-
 def load_shell_commands(dumped: bool = True) -> {'str': 'str'}:
     if dumped:
         with open(MANNAME + '.pickle', 'rb') as man_file:
@@ -115,11 +78,11 @@ def load_shell_commands(dumped: bool = True) -> {'str': 'str'}:
         man_file = open(MANNAME + '.pickle', 'wb')
         pickle.dump(manpages, man_file)
         man_file.close()
-    logging.info('read {} known shell commands'.format(len(manpages.keys())))
+    logging.info('load {} known shell commands in warehouse with manual'.format(len(manpages.keys())))
     return manpages
 
 
-def shell_commands_embedding(manpages: {'str': 'str'}):
+def shell_commands_embedding(manpages: {'str': 'str'}, similar_commands: [str] = []):
     stop_words = get_stop_words('en')
     stemmer = nltk.stem.SnowballStemmer('english')
     commands = []
@@ -146,34 +109,61 @@ def shell_commands_embedding(manpages: {'str': 'str'}):
                 if sim[1] > threshold:
                     print('\t\033[1m' + '{}'.format(commands[sim[0]]) + '\033[0m' + ' ({})'.format(sim[1]))
 
-    find_similar_commands('chmod')
+    for similar_command in similar_commands:
+        find_similar_commands(similar_command)
 
 
-# # TODO: to be redo
-# def parse_commands_from_local():
-#     shell_commands = load_shell_commands()
-#     raw_commands = []
-#     for shell_command in shell_commands:
-#         raw_commands.append(shell_command.raw)
-#
-#     with open(LOGNAME + '.txt') as f:
-#         data = f.readlines()
-#     dataset_commands = []
-#     for line in data:
-#         dataset_commands.append(line.split()[1])
-#     known_commands = []
-#     for dataset_command in dataset_commands:
-#         if dataset_command in raw_commands:
-#             known_commands.append(dataset_command)
-#     logging.info('remaining {} commands known'.format(len(known_commands)))
-#     return known_commands
+def load_dataset(manpages: {'str': 'str'}, multithread: bool = False, windows: int = 10):
+    def read_dataset(period: str):
+        logging.info('reading dataset from json in following period: {}'.format(period))
+        sessions = []
+        for root, dirs, files in os.walk(LOGPATH + '/' + period):
+            for name in files:
+                file_dir = os.path.join(root, name)
+                logging.debug('current read json file: {}'.format(file_dir.split('logs/')[1]))
+                with open(file_dir, encoding='utf-8') as raw_log_file:
+                    data = json.load(raw_log_file)
+                sessions.append(Session(data))
+            log_file = open(LOGNAME + '_' + period + '.pickle', 'wb')
+            pickle.dump(sessions, log_file)
+            log_file.close()
+        logging.info('the size of log is {} MB'.format(sys.getsizeof(sessions) / 1000 / 1000))
+
+    if multithread:
+        pool = []
+        for period in PERIODS:
+            pool.append(threading.Thread(target=read_dataset, args=(period,)))
+        for thread in pool:
+            thread.start()
+        for thread in pool:
+            thread.join()
+
+    commands = defaultdict(int)
+    for period in PERIODS:
+        logging.info('load dataset from pickle in following period: {}'.format(period))
+        with open(LOGNAME + '_' + period + '.pickle', 'rb') as log_file:
+            sessions = pickle.load(log_file)
+        for session in sessions:
+            for activity in session.activities:
+                if activity.command is not None:
+                    if len(activity.command.strip().split()):
+                        commands[activity.command.strip().split()[0]] += 1
+
+    logging.info('current load {0} with {1} unique commands from pickle'.format(sum(commands.values()), len(commands.keys())))
+    known_commands = {}
+    for command in set(commands.keys()) & set(manpages.keys()):
+        known_commands[command] = commands[command]
+    logging.info('already known {0} with {1} unique commands.'.format(sum(known_commands.values()), len(known_commands.keys())))
+
+    
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    # load_dataset(True)
-    # read_shell_commands(False)
-    shell_commands_embedding(load_shell_commands(dumped=True))
+
+    manpages = load_shell_commands(dumped=True)
+    shell_commands_embedding(manpages, similar_commands=['ls', 'rm'])
+    load_dataset(manpages, multithread=False)
 
 
 if __name__ == '__main__':
